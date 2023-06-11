83228cef54bfdbf132e62b4260d89d2fd51ddcdd
turob <tuxtamishovtt@gmail.com>
2023-06-11 23:44:09 +0500

    alert vs telegramBot

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.black"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.telegram:telegrambots-spring-boot-starter:6.1.0")
+
+
+   // Swagger Open API
+    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.7.0")
+
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
